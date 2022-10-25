0406f97cf8604556d83e7b4338fc4c6e3612dc92
mustu43 <116060983+mustu43@users.noreply.github.com>
2022-10-25 16:39:39 +0530

    Add files via upload

---
+++ arthametic.java
@@ -0,0 +1,17 @@
+package javaexamples;
+
+public class arthametic {
+public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int add,sub,mul,div,mod; //creating a integer variables
+		int num1=8,num2=7;  	 // Assign values to int variables 
+		add=num1+num2;
+		div=num1/num2;
+		sub=num1-num2;
+		mul=num1*num2;
+		mod=num1%num2;
+		
+		System.out.println(add+"\n"+sub+"\n"+mul+"\n"+div+"\n"+mod+"\n");
+	}
+
+}
